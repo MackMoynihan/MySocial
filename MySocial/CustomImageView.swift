95a591a6df626bc543fdfd448c431cbbf85563a6
Mack Moynihan <mdm133@pitt.edu>
2017-05-14 11:11:44 -0400

    Tableview UI completed

---
+++ MySocial/CustomImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomImageView.swift
+//  MySocial
+//
+//  Created by Mack Moynihan on 5/14/17.
+//  Copyright © 2017 Mack Moynihan. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class CustomImageView: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat = 30 {
+        didSet {
+            setUpView()
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 2.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
+    func setUpView() {
+        layer.cornerRadius = cornerRadius
+        layer.shadowRadius = shadowRadius
+        layer.shadowOpacity = 1.0
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 1.0).cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setUpView()
+    }
+    
+
+}
---
+++ MySocial/FeedCell.swift
@@ -0,0 +1,32 @@
+//
+//  FeedCell.swift
+//  MySocial
+//
+//  Created by Mack Moynihan on 5/14/17.
+//  Copyright © 2017 Mack Moynihan. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profilePic: CustomImageView!
+    @IBOutlet weak var postLbl: UILabel!
+    @IBOutlet weak var like: UIImageView!
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var likeCount: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
